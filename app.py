2192dd6b445be9b371586a7e60980491e16f4038
Mahendra Mogarala <mogaralamahendra6@gmail.com>
2026-06-23 10:25:16 +0530

    fix deployment

--- app.py
+++ app.py
@@ -3,19 +3,28 @@ from deep_translator import GoogleTranslator
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def home():
     return render_template("index.html")
 
+
 @app.route("/translate", methods=["POST"])
 def translate():
     data = request.get_json()
+
     text = data["text"]
     lang = data["language"]
 
-    result = GoogleTranslator(source='auto', target=lang).translate(text)
+    translated = GoogleTranslator(
+        source="auto",
+        target=lang
+    ).translate(text)
 
-    return jsonify({"translation": result})
+    return jsonify({
+        "translation": translated
+    })
+
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
